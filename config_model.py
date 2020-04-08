fe681dda2913f539103af286d59bafa20f90a66f
tlucky <roterstarr-heute@yahoo.de>
2020-04-08 20:22:45 +0200

    Some changes, scaling of plot not working

--- config_model.py
+++ config_model.py
@@ -25,7 +25,7 @@ from keras.models import Sequential
 class Config:
     def __init__(self, mode='conv', nfilt=40, nfeat=1, nfft=512, sample_rate=16000, 
                  low_freq_mel = 0, pre_emphasis = 0.97, frame_size = 0.025, 
-                 new_len=13856, num_ceps = 12, cep_lifter = 22):
+                 new_len=13856, num_ceps = 12, cep_lifter = 22, freq_fft=10000):
         self.mode = mode
         self.nfilt = nfilt
         self.nfeat = nfeat
@@ -35,11 +35,16 @@ class Config:
         self.pre_emphasis = pre_emphasis
         self.frame_size = frame_size
         self.new_len = new_len
+
         self.num_ceps = num_ceps
         self.cep_lifter = cep_lifter
+        self.freq_fft = freq_fft
+        
         #self.step = int(rate/10)  # hier evtl das auch ändern
         self.model_path = os.path.join('models', mode + '.model')
         self.p_path = os.path.join('pickles', mode + '.p')
+    def len_ms(self):
+        return self.new_len / self.sample_rate * 1000 #ms
 
 class ModelSpec:
     def __init__(self, input_shape):
--- signal_visualisation.py
+++ signal_visualisation.py
@@ -20,18 +20,30 @@ def work_status(begin_str):
         return (1)  # Close
     if begin_str.startswith('Q') == True:
         return (2)  # Error
+
+def rounddown(x, round_to):
+    return int(int(x / round_to)) * round_to
+
+# def rescale_axis(tick, n_ticks = 9, max_new = 800):
+#     """
+#     Calculates the arrays which are needed for resizing the y-axis
+#     """
+#     x_max = tick/config.len_ms()*max_new+1
+#     x_step = tick/config.len_ms()*max_new/(n_ticks-1)
+#     ticks_range = np.arange(0, x_max, x_step) # für 3
+#     labels_range = np.arange(0, max_new+1, max_new/(n_ticks-1), int)
+#     return ticks_range, labels_range
                   
 def plot_wav(signals):
     fig, ax = plt.subplots(nrows=1, ncols=3, sharex=False,
                              sharey=True, figsize=(20,5))
     fig.suptitle('Emphasized Signal', size=16)
-    
-    scale = config.new_len / config.sample_rate * 1000  # ms
-    x_ticklabels = np.arange(0, int(scale),int(scale/8))
-    
-    x_ticks = np.arange(0, config.new_len,int(config.new_len/8))
+    x_tick = signals['Open'].shape[0] 
+    x_ticks, x_ticklabels = rescale_axis(x_tick, 9, 10)
+    # x_ticklabels = np.arange(0, config.len_ms()+1,config.len_ms()/8)
+    # x_ticks = np.arange(0, config.new_len+1,int(config.new_len/8))
 
-    ax[0].set_ylabel('Amplitude [?]')
+    ax[0].set_ylabel('Amplitude')
   
     for y in range(3):
             ax[y].set_title(list(signals.keys())[y])
@@ -74,27 +86,53 @@ def plot_fft_hamming(fft_hamming):
         ax[y].set_xlabel('Frequenz [Hz]')
         ax[y].fill_between(freq, Y)  # Fills the area under the plot
         ax[y].grid(linestyle='-')
-        
+ 
+def rescale_axis(tick, old_max , n_ticks = 9, max_new = 800):
+    """
+    Calculates the arrays which are needed for resizing the y-axis
+    """
+    #
+    ax_max = tick/config.len_ms()*max_new+1
+    step = tick/config.len_ms()*max_new/(n_ticks-1)
+    ticks_range = np.arange(0, ax_max, step) # für 3
+    labels_range = np.arange(0, max_new+1, max_new/(n_ticks-1), int)
+    return ticks_range, labels_range
+
 def plot_stft(mag_frames):        
     fig, ax = plt.subplots(nrows=1, ncols=3, sharex=False,
                              sharey=True, figsize=(20,5))
-    fig.suptitle('STFT', size=16)    
+    fig.suptitle('STFT', size=16)  
     
-    scale = config.new_len / config.sample_rate * 1000  # ms
+    # for the rescaling of the axes
+    x_tick, y_tick = stfts['Open'].shape  
+    
+    x_ticks_range, x_ticklabels_range = rescale_axis(x_tick, n_ticks=9,
+                                                     max_new=800)
+    
+    y_ticks_range, y_ticklabels_range = rescale_axis(y_tick, 6, 10)
+    y_ticks_range *= 86
     #x_ticklabels = np.arange(0, int(scale),int(scale/8))
     #scale = len(signals['Open'])
     #x_ticks = np.arange(0, scale,int(scale/8))
     #x_ticks = np.arange(0, config.new_len,int(config.new_len/8))
+    ax[0].set_yticks(y_ticks_range)
+    ax[0].set_yticklabels(y_ticklabels_range)  # Range from 0 - 10.000 Hz
     ax[0].set_ylabel('Frequenz [Hz]')  
     
     for y in range(3):
         ax[y].set_title(list(stfts.keys())[y])
         im = ax[y].imshow(list(stfts.values())[y].T,#extent=[0,T,0,4],
                        cmap=plt.cm.jet, aspect='auto',vmin=0.0,vmax=17.5) # 
+        #  x-axis
+        ax[y].set_xlim(0, x_tick-1)
+        ax[y].set_xticks(x_ticks_range)
+        ax[y].set_xticklabels(x_ticklabels_range)
+        ax[y].set_xlabel('Zeit [ms]')
+        #  y-axis
         ax[y].invert_yaxis()
-        #ax[y].set_xticks(x_ticks)
-        #ax[y].set_xticklabels(x_ticklabels)
         ax[y].set_xlabel('Zeit [ms]') 
+        ax[y].set_ylim(0, y_tick)
+
     fig.colorbar(im,ax=ax[2])  # shows the colorbar one
     #plt.show()
         
@@ -197,12 +235,13 @@ for c in classes:
     mfccs[c] = mfcc      
     fbanks_norm[c]=fbnorm
 
+
 #  Plotting    
 plot_wav(signals)
-plot_fft(ffts)
-plot_fft_hamming(ffts_hamming)
+# plot_fft(ffts)
+# plot_fft_hamming(ffts_hamming)
 plot_stft(stfts)
-plot_banks(fbanks)
+# plot_banks(fbanks)
 # plot_banks_norm(fbanks_norm)
 # plot_mfcc(mfccs)
 
